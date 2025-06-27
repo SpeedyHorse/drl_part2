c9465d18c447d7ee1180f9bcbdb6a150d0b7cd4c
liverHawk <hawkyofkn5365@gmail.com>
2025-06-28 08:48:26 +0900

    done

--- 01_process/004_gan.py
+++ 01_process/004_gan.py
@@ -8,7 +8,9 @@ import torch.nn.functional as F
 import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader, Dataset
 import time
+from datetime import datetime
 
+MAX_TIME = 2 * 60
 
 Z_DIM = 100
 NUM_EPOCHS = 10_000
@@ -66,7 +68,7 @@ def plot_losses(d_losses, g_losses, title):
     plt.plot(g_losses, label="Generator Loss")
     plt.title(title)
     plt.legend()
-    plt.savefig(f"losses_{title}.png")
+    plt.savefig(f"result/gan/losses.png")
 
 
 def convert_to_data(df):
@@ -129,41 +131,33 @@ def train_gan(df):
         dataset,
         batch_size=BATCH_SIZE,
         shuffle=True,
-        num_workers=os.cpu_count(),
-        pin_memory=True,
+        # num_workers=os.cpu_count(),
+        # pin_memory=True,
     )
 
     data_column_size = len(feature_cols)
     gen_hidden_size = disc_hidden_size = HIDDEN_SIZE
     gen_lr = disc_lr = LEARNING_RATE
 
-    gen_model = Generator(Z_DIM, data_column_size, gen_hidden_size)
-    disc_model = Discriminator(data_column_size, disc_hidden_size)
+    while True:
+        gen_model = Generator(Z_DIM, data_column_size, gen_hidden_size)
+        disc_model = Discriminator(data_column_size, disc_hidden_size)
 
-    # PyTorch 2.0+ であれば torch.compile でモデルを高速化
-    if hasattr(torch, "compile"):
-        gen_model = torch.compile(gen_model)
-        disc_model = torch.compile(disc_model)
+        # PyTorch 2.0+ であれば torch.compile でモデルを高速化
+        if hasattr(torch, "compile"):
+            gen_model = torch.compile(gen_model)
+            disc_model = torch.compile(disc_model)
 
-    gen_optimizer = optim.Adam(gen_model.parameters(), lr=gen_lr)
-    disc_optimizer = optim.Adam(
-        disc_model.parameters(), 
-        lr=disc_lr,
-        betas=(0.5, 0.999),
-    )
+        gen_optimizer = optim.Adam(gen_model.parameters(), lr=gen_lr)
+        disc_optimizer = optim.Adam(
+            disc_model.parameters(), 
+            lr=disc_lr,
+            betas=(0.5, 0.999),
+        )
 
-    d_losses = []
-    g_losses = []
+        d_losses = []
+        g_losses = []
 
-    if len(dataloader) < 10:
-        total_epochs = 100
-    elif len(dataloader) < 1000:
-        total_epochs = 20
-    else:
-        total_epochs = 5
-
-
-    for epoch in range(total_epochs):
         start_time = time.time()
         for i, real_data in enumerate(dataloader):
             current_batch_size = real_data.size(0)
@@ -175,13 +169,16 @@ def train_gan(df):
             
             z = torch.randn(current_batch_size, Z_DIM)
             fake_data = gen_model(z)
+            fake_data_clone = fake_data.clone()
+            fake_data_clone[:, 0] = torch.round(fake_data_clone[:, 0] * 65535)
+            fake_data_clone[:, 1] = torch.round(fake_data_clone[:, 1] * 17)
 
             # 本物データに対する損失
             real_output = disc_model(real_data)
             d_loss_real = F.binary_cross_entropy(real_output, torch.ones_like(real_output))
 
             # 偽データに対する損失 (Generatorの勾配は計算しない)
-            fake_output = disc_model(fake_data.detach())
+            fake_output = disc_model(fake_data_clone.detach())
             d_loss_fake = F.binary_cross_entropy(fake_output, torch.zeros_like(fake_output))
 
             # Discriminatorの損失
@@ -205,18 +202,81 @@ def train_gan(df):
 
             if (i + 1) % 1_000 == 0:
                 time_elapsed = time.time() - start_time
-                print(f"\rEpoch {epoch + 1:7d} / {total_epochs:7d} | Step {i + 1:10d} / {len(dataloader):10d} | Time elapsed: {time_elapsed:.2f}s", end="")
-                # print(f"\rEpoch {epoch + 1:10d} / {NUM_EPOCHS:10d} | Step {i + 1:10d} / {len(dataloader):10d} | D Loss: {d_loss.item():.4f} | G Loss: {g_loss.item():.4f}", end="")
+                print(f"\rStep {i + 1:10d} / {len(dataloader):10d} | D Loss: {d_loss.item():.4f} | G Loss: {g_loss.item():.4f}", end="")
 
         end_time = time.time()
         print("\r" + " " * 100, end="")
-        print(f"\rEpoch {epoch + 1:7d} / {total_epochs:7d} | Time: {end_time - start_time:.2f}s")
+        print(f"\rFirst Epoch | Time: {end_time - start_time:.2f}s | D Loss: {d_loss.item():.4f} | G Loss: {g_loss.item():.4f}")
+        if d_losses[-1] < 100:
+            print(f"---d_losses[-1] < 100---")
+            break
 
-        if (epoch + 1) % 10_000 == 0:
+    total_epochs = int(MAX_TIME // (end_time - start_time) + 1)
+
+    phase = total_epochs // 10
+    if phase == 0:
+        phase = 1
+
+    for epoch in range(total_epochs):
+        start_time = time.time()
+        for i, real_data in enumerate(dataloader):
+            current_batch_size = real_data.size(0)
+
+            # ---------------------
+            #  Discriminatorの学習
+            # ---------------------
+            disc_optimizer.zero_grad()
+            
+            z = torch.randn(current_batch_size, Z_DIM)
+            fake_data = gen_model(z)
+            fake_data_clone = fake_data.clone()
+            fake_data_clone[:, 0] = torch.round(fake_data_clone[:, 0] * 65535)
+            fake_data_clone[:, 1] = torch.round(fake_data_clone[:, 1] * 17)
+
+            # 本物データに対する損失
+            real_output = disc_model(real_data)
+            d_loss_real = F.binary_cross_entropy(real_output, torch.ones_like(real_output))
+
+            # 偽データに対する損失 (Generatorの勾配は計算しない)
+            fake_output = disc_model(fake_data_clone.detach())
+            d_loss_fake = F.binary_cross_entropy(fake_output, torch.zeros_like(fake_output))
+
+            # Discriminatorの損失
+            d_loss = d_loss_real + d_loss_fake
+            d_loss.backward()
+            disc_optimizer.step()
+
+            # -----------------
+            #  Generatorの学習
+            # -----------------
+            gen_optimizer.zero_grad()
+            
+            # Discriminatorの学習で使った偽データを再利用し、Generatorの勾配を計算
+            fake_output_for_g = disc_model(fake_data_clone)
+            g_loss = F.binary_cross_entropy(fake_output_for_g, torch.ones_like(fake_output_for_g))
+            g_loss.backward()
+            gen_optimizer.step()
+
+            d_losses.append(d_loss.item())
+            g_losses.append(g_loss.item())
+
+            if (i + 1) % 1_000 == 0:
+                time_elapsed = time.time() - start_time
+                # print(f"\rEpoch {epoch + 1:7d} / {total_epochs:7d} | Step {i + 1:10d} / {len(dataloader):10d} | Time elapsed: {time_elapsed:.2f}s", end="")
+                print(f"\rEpoch {epoch + 1:7d} / {total_epochs:7d} | Step {i + 1:10d} / {len(dataloader):10d} | D Loss: {d_loss.item():.4f} | G Loss: {g_loss.item():.4f}", end="")
+
+        end_time = time.time()
+        print("\r" + " " * 100, end="")
+        print(f"\rEpoch {epoch + 1:7d} / {total_epochs:7d} | Time: {end_time - start_time:.2f}s | D Loss: {d_loss.item():.4f} | G Loss: {g_loss.item():.4f}")
+
+        if phase > 0 and (epoch + 1) % phase == phase - 1:
             title = f"Epoch {epoch + 1:7d} / {total_epochs:7d}"
             plot_losses(d_losses, g_losses, title)
-            print(f"\rEpoch {epoch + 1:7d} / {total_epochs:7d} | D Loss: {d_loss.item():.4f} | G Loss: {g_loss.item():.4f}")
-    
+            print(f"---loss plot done---")
+    else:
+        title = f"Epoch {epoch + 1:7d} / {total_epochs:7d}"
+        plot_losses(d_losses, g_losses, title)
+
     label = label_list[0]
     torch.save(gen_model.state_dict(), f"result/gan/gen/gen_model_{label}.pth")
     torch.save(disc_model.state_dict(), f"result/gan/disc/disc_model_{label}.pth")
@@ -251,6 +311,7 @@ def convert_to_data(df):
             df[label] = df[label].apply(lambda x: float(x * (max_value - min_value) + min_value))
     return df
 
+
 def generate_data(df, max_size=10_000):
     columns = df.columns.tolist()
     feature_cols = [col for col in columns if col != "Label" and col != "Attempted Category"]
@@ -264,7 +325,7 @@ def generate_data(df, max_size=10_000):
 
     # device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
     device = torch.device("cpu")
-    gen_model = Generator(Z_DIM, data_column_size, HIDDEN_SIZE).to(device)
+    gen_model = Generator(Z_DIM, data_column_size, HIDDEN_SIZE)
     state_dict = load_state_dict(label, device)
     gen_model.load_state_dict(state_dict)
     
@@ -305,6 +366,10 @@ if __name__ == "__main__":
             df_label = df[df["Label"] == label]
             train_gan(df_label)
             print(f"finish train {label}")
+        else:
+            with open("result/gan/train_done.txt", "w") as f:
+                f.write("train done\n")
+                f.write(f"now time: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
     
     if is_generate:
         print("start generate")
